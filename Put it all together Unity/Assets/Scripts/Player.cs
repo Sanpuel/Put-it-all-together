9c8bf003a79bbd8ded2c1515ce2489c9175fbe91
SamP <sam@easkag.net>
2021-01-29 16:46:18 -0800

    added some stuff in the scripts

--- Put it all together Unity/Assets/Scripts/Player.cs
+++ Put it all together Unity/Assets/Scripts/Player.cs
@@ -8,18 +8,22 @@ public class Player : MonoBehaviour
     [SerializeField] float runspeed = 5f;
 
 
-    Rigidbody2D myRigidBody; 
+
+    Rigidbody2D myRigidBody;
+    Animator myanimator;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myanimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     private void Run()
@@ -27,5 +31,17 @@ public class Player : MonoBehaviour
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(controlThrow * runspeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myanimator.SetBool("Running", playerHasHorizontalSpeed);
     }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+           transform.localScale= new Vector2(2*Mathf.Sign(myRigidBody.velocity.x), 2f);
+        }
+    } 
 }
